e2336aea8297c8e8d443120f5021974a6523209e
14wqlin <970944836@qq.com>
2026-06-18 20:44:12 +0800

    Update 3 _Source_Target_Regression-Ea.py

--- 3 _Source_Target_Regression-Ea.py
+++ 3 _Source_Target_Regression-Ea.py
@@ -194,13 +194,13 @@ def GBRegression(trainX, testX, trainY, testY, max_evals, Loop_Step, screen_step
 
     # Define the parameter space for hyperopt ## need to be modified to a wider range
     parameter_space_gbr = {
-        "colsample_bytree": hp.uniform("colsample_bytree", 0.5, 1.0),
-        "max_depth": hp.quniform("max_depth", 1, 10, 1),
-        "n_estimators": hp.quniform("n_estimators", 10, 200, 1),
-        "learning_rate": hp.uniform("learning_rate", 0.01, 0.5),
-        "subsample": hp.uniform("subsample", 0.5, 1.0),
-        "min_child_weight": hp.uniform("min_child_weight", 0.5, 10),
-        "gamma": hp.uniform("gamma", 0.01, 1)
+        "colsample_bytree": hp.uniform("colsample_bytree", 0.685, 0.705),
+        "max_depth": hp.quniform("max_depth", 6, 8, 1),
+        "n_estimators": hp.quniform("n_estimators", 180, 200, 1),
+        "learning_rate": hp.uniform("learning_rate", 0.47, 0.49),
+        "subsample": hp.uniform("subsample", 0.72, 0.75),
+        "min_child_weight": hp.uniform("min_child_weight", 1.42, 1.44),
+        "gamma": hp.uniform("gamma", 0.30, 0.33)
     }
 
     count = 0  # Counter for parameter combinations
